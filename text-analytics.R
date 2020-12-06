26f0849b0817747a7cced2593173285ebe43b5bf
Michael Frank <frankm@dev1.mike.com>
2020-12-06 11:09:15 -0500

    Longest words and sentences

--- text-analytics.R
+++ text-analytics.R
@@ -6,9 +6,23 @@ source("text-functions.R")
 
 tarzan.file <- here("data", "TarzanOfTheApes.txt")
 tarzan.lines <- book_to_vector(tarzan.file, remove_last = 3)
-tarzan.lines <- book_to_chapters(tarzan.lines)
+tarzan.chapters <- book_to_chapters(tarzan.lines)
+chapter_names <- get_chapter_names(tarzan.lines)
 
-vcorpus <- VCorpus(VectorSource(tarzan.lines))
-dtm <- DocumentTermMatrix(vcorpus)
-vcorpus
-inspect(dtm)
+lecuture_functions(tarzan.lines)
+
+tarzan.frame <- data.frame(chapter=chapter_names, text=tarzan.chapters)
+str(tarzan.frame)
+
+tarzan.frame %>%
+  unnest_tokens(word, text) %>%
+  mutate(word_size=nchar(word)) %>%
+  arrange(desc(word_size)) %>%
+  top_n(5)
+
+tarzan.frame %>%
+  unnest_tokens(sentence, text, token = "sentences") %>%
+  mutate(sentence_len=nchar(sentence)) %>%
+  arrange(desc(sentence_len)) %>%
+  top_n(5)
+
--- text-functions.R
+++ text-functions.R
@@ -1,6 +1,6 @@
 library(here)
 library(tm)
-#library(tidyverse)
+library(tidyverse)
 
 book_to_vector <- function(path, encoding = "UTF-8", display_count = 10, remove_last = 0){
   stopifnot(file.exists(path))
@@ -12,7 +12,7 @@ book_to_vector <- function(path, encoding = "UTF-8", display_count = 10, remove_
   book.lines <- book.lines[which(book.lines != "")]
   
   # Grab content from first chapter to end of book
-  start_index <- grep("^chapter", book.lines, ignore.case = TRUE)[1] + 1
+  start_index <- grep("^chapter", book.lines, ignore.case = TRUE)[1]
   end_index <- grep("^end of project gutenberg's", book.lines, ignore.case = TRUE) - 1 - remove_last
   book.lines <- book.lines[start_index:end_index]
   
@@ -23,17 +23,29 @@ book_to_vector <- function(path, encoding = "UTF-8", display_count = 10, remove_
   book.lines
 }
 
-book_to_chapters <- function(book.lines){
+get_chapter_names <- function(book.lines, chapter_regex = "Chapter [IVXLCDM]+"){
+  chapter_lines <- grep(chapter_regex, book.lines)
+  titles <- chapter_lines + 1
+  book.lines[titles]
+}
+
+book_to_chapters <- function(book.lines, chapter_regex = "Chapter [IVXLCDM]+"){
+  #book.lines <- book.lines
+  
   # Get the index of each chapter, the next line is the title
-  chapter_lines <- grep("Chapter [IVXLCDM]+", book.lines)
+  chapter_lines <- grep(chapter_regex, book.lines)
   titles <- chapter_lines + 1
   # Remove titles
   book.lines <- book.lines[-titles]
   book.lines <- book.lines[-1]
   
+  # Split by chapter duplicate white space
   single_line <- paste(book.lines, collapse = ' ')
   chapters <- strsplit(single_line, "Chapter [IVXLCDM]+", fixed = FALSE, perl = TRUE)
   chapters <- lapply(chapters, trimws)[[1]]
+  for(i in 1:length(chapters)){
+    chapters[i] <- str_replace_all(chapters[i], "[ ]+", " ")
+  }
   message("Displaying book by chapters")
   print(chapters)
   chapters
@@ -41,4 +53,21 @@ book_to_chapters <- function(book.lines){
 
 lecuture_functions <- function(book.vector){
   vcorpus <- VCorpus(VectorSource(book.vector))
+  dtm <- DocumentTermMatrix(vcorpus)
+  print(vcorpus)
+  print(inspect(dtm))
+}
+
+longest <- function(book.frame, n = 5){
+  longest.words <- book.frame %>%
+    unnest_tokens(word, text) %>%
+    mutate(word_size=nchar(word)) %>%
+    arrange(desc(word_size)) %>%
+    top_n(n)
+  
+  longest.sentences <- book.frame %>%
+    unnest_tokens(sentence, text, token = "sentences") %>%
+    mutate(sentence_len=nchar(sentence)) %>%
+    arrange(desc(sentence_len)) %>%
+    top_n(n)
 }
